3d74e34cb93569eeaa8930738d4616c7346f0fdb
irasally <irasally@gmail.com>
2013-12-09 16:51:48 +0900

    create about page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,4 +13,10 @@ describe "StaticPages" do
       expect(page).to have_content('Help')
     end
   end
+  describe "About Page" do
+    it "shuld have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
