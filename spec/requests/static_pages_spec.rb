d80d71089146aed70127c66713045999c0cdc3b6
irasally <irasally@gmail.com>
2013-12-09 17:07:29 +0900

    各々のページに異なるタイトルを表示させる

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,31 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "shuld have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('RUby on Rails Tutorial Sample App | Home')
+    end
   end
+
   describe "Help Page" do
     it "shuld have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "shuld have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('RUby on Rails Tutorial Sample App | Help')
+    end
   end
+
   describe "About Page" do
     it "shuld have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "shuld have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('RUby on Rails Tutorial Sample App | About Us')
     end
   end
 end
