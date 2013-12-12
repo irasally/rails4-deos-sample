f3123cec927c3ccbbfb0b9a1c5854136c04c01f4
irasally <irasally@gmail.com>
2013-12-12 15:50:46 +0900

    add secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: 'Omuko', email: 'omuco@example.com')
+    @user = User.new(name: 'Omuko', email: 'omuco@example.com', password: 'foobar', password_confirmation: 'foobar')
   end
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email)}
+  it { should respond_to(:password_digest)}
+  it { should respond_to(:password)}
+  it { should respond_to(:password_confirmation)}
   it { should be_valid}
 
   describe "when name is not present" do
@@ -15,8 +18,8 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email is not present" do
-    before { @user.email = " " }
+  describe "when password is not present" do
+    before { @user.name = " " }
     it { should_not be_valid }
   end
 
@@ -51,6 +54,18 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: 'Omuko', email: 'omuco@example.com', password: ' ', password_confirmation: ' ')
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = 'missmatch' }
     it { should_not be_valid }
   end
 end
