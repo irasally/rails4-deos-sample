d0a72e4304c73f144a7fef97b7dc5e47a0a6bff0
irasally <irasally@gmail.com>
2013-12-13 15:51:01 +0900

    fill_inで指定するラベル名が誤っていた

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,10 +23,10 @@ describe "UserPages" do
     end
     describe "with valid information" do
       before do
-        fill_in "Name",            with: "Example User"
-        fill_in "Email",           with: "user@example.com"
-        fill_in "Password",        with: "foobar"
-        fill_in "PasswordConfirm", with: "foobar"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
       it "should create a user" do
         expect{ click_button submit }.to change(User, :count).by(1)
