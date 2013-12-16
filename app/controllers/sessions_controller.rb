bbf90317abb8956897078fccacb81abd19027815
irasally <irasally@gmail.com>
2013-12-16 17:19:45 +0900

    ユーザ作成時にremember_tokenを生成する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # signin success
+      sign_in user
+      redirect_to user
     else
       # invalid access
       flash.now[:error] = 'Invalid email/password combination'
