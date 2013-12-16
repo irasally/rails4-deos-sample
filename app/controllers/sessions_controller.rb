b8e30b6ddbb7a58976c11db97d4c3d95070dc572
irasally <irasally@gmail.com>
2013-12-16 16:33:45 +0900

    サインイン失敗処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
   def new
   end
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # signin success
+    else
+      # invalid access
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   def destroy
   end
