7a07a2163c8cf20b9cae99f2bae6edf8d51309e8
irasally <irasally@gmail.com>
2013-12-17 15:41:43 +0900

    サインインの残テストを追加

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -2,10 +2,39 @@
 前提(/^サインインページを表示している$/) do
   visit signin_path
 end
+
+前提(/^ユーザは登録済みである$/) do
+  @user = User.create(name: 'test', email: 'user@example.com', password: 'foobar', password_confirmation: 'foobar')
+end
+
 もし(/^何も入力せずにログインボタンを押す$/) do
-  click_button "Sign in"
+  sign_in
 end
 
 ならば(/^エラーメッセージが表示される$/) do
   expect(page).to have_selector('div.alert.alert-error')
+end
+
+もし(/^存在しないユーザ情報を入力してログインボタンを押す$/) do
+  fill_in "Email", with: 'Hogehoge@example.com'
+  fill_in "Password", with: 'Papapapapa'
+  sign_in
+end
+
+もし(/^存在するユーザ情報を入力してログインボタンを押す$/) do
+  fill_in "Email", with: @user.email
+  fill_in "Password", with: @user.password
+  sign_in
+end
+
+ならば(/^プロフィールページが表示される$/) do
+  expect(page).to have_title(@user.name)
+end
+
+ならば(/^サインアウトのリンクが存在する$/) do
+  expect(page).to have_link('Sign out', href: signout_path)
+end
+
+def sign_in
+  click_button "Sign in"
 end
