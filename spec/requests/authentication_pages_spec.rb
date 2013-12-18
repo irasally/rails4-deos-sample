1d5b3227828864985ec73ec4a63af032d40bffc1
irasally <irasally@gmail.com>
2013-12-18 17:21:07 +0900

    認証されたユーザが自身の情報を編集できるようにする

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,6 +8,7 @@ describe "AuthenticationPages" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
   describe "signin" do
     before{  visit signin_path }
     describe "with invalid information" do
@@ -21,14 +22,31 @@ describe "AuthenticationPages" do
     end
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user) }
+      before { sign_in user }
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+      end
+    end
+
+    describe "authorization" do
+      describe "for non-sign-in users" do
+        let(:user) {  FactoryGirl.create(:user) }
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
     end
   end
