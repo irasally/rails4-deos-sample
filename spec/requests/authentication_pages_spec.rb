44db53aaf61e2819ae7b0dcbdd3ee92594a01895
irasally <irasally@gmail.com>
2013-12-19 11:45:28 +0900

    headerメニューにユーザ一覧へ遷移するリンクを追加

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,9 +24,10 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
       it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Users',       href: users_path)}
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       describe "followed by signout" do
         before { click_link "Sign out" }
