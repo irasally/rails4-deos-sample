cce767e25bb6c435d151efd300c3558d076812c4
irasally <irasally@gmail.com>
2013-12-19 13:46:29 +0900

    sequenctを使用してサンプルユーザーを複数作成できるようにした

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "Omuko"
-    email "irasally@gmail.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
   end
