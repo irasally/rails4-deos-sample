76acdee7905d3524ba632e1412fcef002ad552a4
irasally <irasally@gmail.com>
2013-12-20 17:23:59 +0900

    userとmicropostsの関連づけ

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,12 +3,14 @@ require 'spec_helper'
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
   before do
-    @micropost = Micropost.new(content: "Hello my world", user_id: user.id)
+    @micropost = user.microposts.build(content: "Hello my world")
   end
   subject { @micropost }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
 
   it {  should be_valid }
 
