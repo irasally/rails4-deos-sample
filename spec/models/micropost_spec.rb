37c96523fcae8b25aee5fd552e5738ee8ecc924b
irasally <irasally@gmail.com>
2013-12-20 17:17:09 +0900

    create micropost model.

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Micropost do
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = Micropost.new(content: "Hello my world", user_id: user.id)
+  end
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+end
