04a88fc36e76e54469a41fbe9b2bb69a7a4951e3
irasally <irasally@gmail.com>
2014-01-14 14:36:15 +0900

    feed取得のSQLに誤りがあったので修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,9 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
 
   def self.from_users_followed_by(user)
-    followed_user_ids = "SELECT followerd_id FROM relationships
+    followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
-    self.where("user_id IN (:followed_user_ids) OR user_id = :user_id",
-                followed_user_ids: followed_user_ids, user_id: user)
+    self.where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+                user_id: user.id)
   end
 end
